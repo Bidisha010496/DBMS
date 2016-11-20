1b8e1bfb8b4cca123b95feb3a06a99cd6939b994
Bidisha <bidisha.bksh@gmail.com>
2016-11-20 20:19:08 +0530

    change3

--- landing.php
+++ landing.php
@@ -20,11 +20,11 @@
 <head>
 	<title>ProjectHub | Welcome</title>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
     <script type="text/javascript" src="jquery/jquery.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 
 <body>
@@ -63,71 +63,63 @@
 	<div class="col-md-5" id="posts">
 		<?php
 		$txt=$_GET["id"];
-		$sql = "SELECT * FROM project where category_id='".$txt."'" ;
-        $result = $conn->query($sql);
-      //  $row=mysqli_fetch_row($result);
-		$index = 0;
-		while($row=mysqli_fetch_assoc($result))	
-	{
-		$index = $index + 1;
-		?>
+		$sql1 = "SELECT * FROM category where category_id='".$txt."'" ;
+        $result1 = $conn->query($sql1);
+        $row1=mysqli_fetch_assoc($result1)
+        ?>
+		    <div id="post1">
+		        <div class="row">
+					<div class="col-md-9 protitle" id="pro-title">Category Title:<?php echo $row1["category_name"];?></div><br><hr>
+						<div class="row description" id="pro-desc">
+							 Description in 180 words. I think this much description will be enough to get an idea about the project. If you need then we can increase the length but the div height will increase. 
+						</div>
+				</div>
+			</div>        
+	       		  <?php
+					$sql= "SELECT * FROM project where category_id='".$txt."'" ;
+        			$result = $conn->query($sql);
+      				//  $row=mysqli_fetch_row($result);
+					$index = 0;
+					while($row=mysqli_fetch_assoc($result))	
+					{
+						$index = $index + 1;
+				  ?>
 	
-	
-		<a><div id="post1" >	
-			<div class="row">
-				<div class="col-md-9 protitle" id="pro-title">Project Title:<?php echo $row["project_title"];?></div>
-				<div class="col-md-1" id="pro-status">Status:<?php echo $row["status"];?></div>
-			</div><hr>
-			<div class="row description" id="pro-desc">
-				 <!--Description in 180 words. I think this much description will be enough to get an idea about the project. If you need then we can increase the length but the div height will increase. -->
-				 <?php echo $row["description"];?>
-			</div>
-		</div></a>
-		<?php
-	}
-	?>
-
-	 <?php 
-	/*$index = 0;
-	foreach ($row as $key => $value) {
-		$index = $index + 1;
-		// var_dump($row['proj_title']);
-		printf("<a href=''><div id='post_".$index."'>");
-		printf("<div class='row'>");
-				printf("<div class='col-md-10 protitle' id='pro-title'>".$row['proj_title']."</div>");
-				printf("<div class='col-md-1' id='pro-status'>".$row['status']."</div>");
-			printf("</div><hr>");
-			printf("<div class='row description' id='pro-desc'>");
-			printf ($row['description']);
-			printf("</div>");
-		printf("</div></a>");*/
-		/*$txt=$_GET["id"];
-		$sql = "SELECT * FROM project where Project_id='".$txt."'" ;
-        $result = $conn->query($sql);
-      //  $row=mysqli_fetch_row($result);
-		$index = 0;
-
-	while($row=mysqli_fetch_assoc($result))	
-	{
-		$index = $index + 1;
-		// var_dump($row['proj_title']);
-
-		echo  "<a href=''><div id='post_".$index."'>";
-		echo "<div class='row'>";
-		echo "<div class='col-md-10 protitle' id='pro-title'>".$row["Project_id"]."</div>";
-				echo "<div class='col-md-1' id='pro-status'>".$row["status"]."</div>";
-			echo "</div><hr>";
-			echo "<div class='row description' id='pro-desc'>";
-			echo $row["description"];
-			echo "</div>";
-		echo "</div></a>" ;
-
-}*/
-
-
-	 ?> 
+	<div id="post1" >	
+		<div class="row">
+			<div class="col-md-9 protitle" id="pro-title">Project Title:<?php echo $row["project_title"];?></div>
+			<div class="col-md-1" id="pro-status">Status:<?php echo $row["status"];?></div>
+		</div><hr>
+		<div class="row description" id="pro-desc">
+			 <!--Description in 180 words. I think this much description will be enough to get an idea about the project. If you need then we can increase the length but the div height will increase. -->
+			 <?php echo $row["description"];?>
+		</div><br><hr>
+			<button type="button" data-toggle="modal" data-target="#myModal">Request Joining</button><br><br>
+		
+			<!-- <button type="button"  data-toggle="modal" data-target="#myModal">Request Joining</button>
+	 		    <div class="modal fade" id="myModal" role="dialog">
+	  	     		<div class="modal-dialog">
+	      				<div class="modal-content">
+	        				<div class="modal-header">
+	          					<button type="button" class="close" data-dismiss="modal">&times;</button>
+	          					<h4 class="modal-title">Are you sure?</h4>
+	        				</div>
+	        				<div class="modal-body">
+	          					<p>We are sending a request to the owner of the project.</p>
+	        				</div>
+	        				<div class="modal-footer">
+	        				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	        					<form action="http://localhost/ProjectHub-master/sendRequest.php" > <button type="submit">OK </button></form>
+	        				</div>
+	      				</div>
+	      			</div>
+				</div>  -->
+				<?php	
+				}
+				?>
 	</div>
-
+	</div>
+	</div>	
 	<div class="col-md-5" id="full_details">
 		<div id="add-project">
 			<center><h2>Wow! Tell Us About Your Project</h2></center><br>
@@ -171,19 +163,14 @@
 		</div>
 		<div id="project-full-descp">
 			<div class="row">
-				<div class="col-md-10 protitle" id="full-pro-title">Project Title</div>
-				<div class="col-md-1" id="full-pro-status">Status</div>
+				<div class="col-md-10 protitle" id="full-pro-title">Project Title:<?php echo $row1["category_name"];?></div>
+				<div class="col-md-1" id="full-pro-status">Status:Running</div>
 			</div><hr>
 			<div class="row description" id="full-pro-desc">Full Description here</div><hr>
-			<div class="row buttons">
-				<button>Join</button>
-				<button>Request</button>
-			</div>
+			 <?php echo $row1["details"];?>
 		</div>
 
-	</div>
-</div>
-
+  </div>
 </body>
 <script type="text/javascript">
 $(document).ready(function(){
@@ -195,10 +182,11 @@ $(document).ready(function(){
     $("#post1").click(function(){
     	$("#add-project").toggle(1000);
         $("#project-full-descp").toggle(1000);
-        document.getElementById('full-pro-title').innerHTML = document.getElementById('pro-title').innerHTML;
-        document.getElementById('full-pro-status').innerHTML = document.getElementById('pro-status').innerHTML;
-        document.getElementById('full-pro-desc').innerHTML = document.getElementById('pro-desc').innerHTML;
+       // document.getElementById('full-pro-title').innerHTML = document.getElementById('pro-title').innerHTML;
+        // document.getElementById('full-pro-status').innerHTML = document.getElementById('pro-status').innerHTML;
+        // document.getElementById('full-pro-desc').innerHTML = document.getElementById('pro-desc').innerHTML;
     });
+
 });
 
 </script>
